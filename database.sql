9129fb4745641061fd3bbff4f2473618e2febd9f
Jules Simon <jules91@gmx.com>
2025-04-15 16:40:08 +0200

    mysql --> sqlserver

--- database.sql
+++ database.sql
@@ -1,18 +1,30 @@
-CREATE DATABASE IF NOT EXISTS livredor 
-CHARACTER SET utf8mb4 
-COLLATE utf8mb4_general_ci;
+-- Création de la base de données
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'livredor')
+BEGIN
+    CREATE DATABASE livredor;
+END
+GO
 
+-- Utilisation de la base
 USE livredor;
+GO
 
-CREATE TABLE IF NOT EXISTS messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    message TEXT NOT NULL,
-    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+-- Création de la table
+IF OBJECT_ID('dbo.messages', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.messages (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        nom NVARCHAR(255) NOT NULL,
+        message NVARCHAR(MAX) NOT NULL,
+        date DATETIME NOT NULL DEFAULT GETDATE()
+    );
+END
+GO
 
-INSERT INTO messages (nom, message, date) VALUES
-    ('Jules SIMON', 'Ceci est un message test', '2025-04-14 10:00:00'),
-    ('Alban CABADET-BOGDANSKI', 'Youhouuuu ça fonctionne !', '2025-04-14 10:10:00'),
-    ('Rafaël FRON', 'Reste plus qu\'à déployer dans Azure', '2025-04-14 10:15:00'),
-    ('Romain LENOIR', 'Je vais vous mettre 20 les gars, vous êtes trop forts !', '2025-04-14 10:20:00');
+-- Insertion des données
+INSERT INTO dbo.messages (nom, message, date) VALUES
+    (N'Jules SIMON', N'Ceci est un message test', '2025-04-14 10:00:00'),
+    (N'Alban CABADET-BOGDANSKI', N'Youhouuuu ça fonctionne !', '2025-04-14 10:10:00'),
+    (N'Rafaël FRON', N'Reste plus qu''à déployer dans Azure', '2025-04-14 10:15:00'),
+    (N'Romain LENOIR', N'Je vais vous mettre 20 les gars, vous êtes trop forts !', '2025-04-14 10:20:00');
+GO
