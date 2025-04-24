f282ec0ebcb626f420391bba3715274cf049a46a
Jules Simon <114732994+jaille75014@users.noreply.github.com>
2025-04-24 12:25:29 +0200

    Update init_database.php

--- back/init_database.php
+++ back/init_database.php
@@ -5,7 +5,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:sql-livredor-prod-northeurope-01.database.windows.net,1433; Database = sqldb-livredor-prod-northeurope-01", "admAJR", "Cisco!00");
+    $conn = new PDO("sqlsrv:server = tcp:sql-livredor-prod-northeurope-01.database.windows.net,1433; Database = sqldb-livredor-prod-northeurope-01", "esgiAdmin", "Cisco!00");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = file_get_contents('../database.sql');
