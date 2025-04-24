78f4b4a9c22c58756b5622d6aa82cbb0f8323896
Albann0 <131862757+Albann0@users.noreply.github.com>
2025-04-25 00:27:09 +0200

    Update bdd.php

--- config/bdd.php
+++ config/bdd.php
@@ -1,14 +1,18 @@
 <?php
-try {
-    $conn = new PDO("sqlsrv:server = tcp:livredor-sql-serv.database.windows.net,1433; Database = livredor-sql-db", "admAJR", "Cisco!00");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+$serverName = "tcp:sql-livredor-prod-northeurope-01.database.windows.net,1433";
+$connectionOptions = array(
+    "Database" => "sqldb-livredor-prod-northeurope-01", // C'était incorrect dans ta version
+    "Uid" => "esgiAdmin",
+    "PWD" => "Cisco!00",
+    "Encrypt" => 1,
+    "TrustServerCertificate" => 0,
+    "LoginTimeout" => 30
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true)); // Affiche les erreurs si échec
 }
 
-$connectionInfo = array("UID" => "admAJR", "pwd" => "Cisco!00", "Database" => "livredor-sql-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:livredor-sql-serv.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
