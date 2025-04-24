81102843fbdd810cf612581e283e41bb579a133e
Albann0 <131862757+Albann0@users.noreply.github.com>
2025-04-25 00:44:17 +0200

    Update init_database.php

--- back/init_database.php
+++ back/init_database.php
@@ -16,8 +16,24 @@ if ($conn === false) {
     die(print_r(sqlsrv_errors(), true)); // Affiche les erreurs si échec
 }
 
-$sql = file_get_contents('../database.sql');
+//$sql = file_get_contents('../database.sql');
+$sql="-- Création de la table si elle n'existe pas
+IF OBJECT_ID('dbo.messages', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.messages (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        nom NVARCHAR(255) NOT NULL,
+        message NVARCHAR(MAX) NOT NULL,
+        date DATETIME NOT NULL DEFAULT GETDATE()
+    );
+END
 
+-- Insertion des données
+INSERT INTO dbo.messages (nom, message, date) VALUES
+    (N'Jules SIMON', N'Ceci est un message test', '2025-04-14 10:00:00'),
+    (N'Alban CABADET-BOGDANSKI', N'Youhouuuu ça fonctionne !', '2025-04-14 10:10:00'),
+    (N'Rafaël FRON', N'Reste plus qu''à déployer dans Azure', '2025-04-14 10:15:00'),
+    (N'Romain LENOIR', N'Je vais vous mettre 20 les gars, vous êtes trop forts !', '2025-04-14 10:20:00');";
 
 $getResults = sqlsrv_query($conn, $sql);
 if ($getResults === false) {
