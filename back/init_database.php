1d7c1ddb30525dbc394f6c771fde86d3e1434013
Albann0 <131862757+Albann0@users.noreply.github.com>
2025-04-24 12:17:48 +0200

    Update init_database.php

--- back/init_database.php
+++ back/init_database.php
@@ -1,11 +1,11 @@
 <?php
-$connectionInfo = array("UID" => "admAJR", "pwd" => "Cisco!00", "Database" => "livredor-sql-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:livredor-sql-serv.database.windows.net,1433";
+$connectionInfo = array("UID" => "esgiAdmin", "pwd" => "Cisco!00", "Database" => "sqldb-livredor-prod-northeurope-01", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sql-livredor-prod-northeurope-01.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:livredor-sql-serv.database.windows.net,1433; Database = livredor-sql-db", "admAJR", "Cisco!00");
+    $conn = new PDO("sqlsrv:server = tcp:sql-livredor-prod-northeurope-01.database.windows.net,1433; Database = sqldb-livredor-prod-northeurope-01", "admAJR", "Cisco!00");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = file_get_contents('../database.sql');
