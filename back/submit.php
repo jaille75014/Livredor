fecde0524c12b8665d33736ce304d1a1ba9261ff
Albann0 <131862757+Albann0@users.noreply.github.com>
2025-04-25 00:58:22 +0200

    Update submit.php

--- back/submit.php
+++ back/submit.php
@@ -5,7 +5,22 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 header('Content-Type: application/json');
+$serverName = "tcp:sql-livredor-prod-northeurope-01.database.windows.net,1433";
+$connectionOptions = array(
+    "Database" => "sqldb-livredor-prod-northeurope-01", // C'était incorrect dans ta version
+    "Uid" => "esgiAdmin",
+    "PWD" => "Cisco!00",
+    "Encrypt" => 1,
+    "TrustServerCertificate" => 0,
+    "LoginTimeout" => 30
+);
 
+// Établir la connexion
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true)); // Affiche les erreurs si échec
+}
 
 
 
@@ -18,16 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    try {
-        $query = "INSERT INTO messages (nom, message, date) VALUES (?, ?, NOW())";
-        $req = $pdo->prepare($query);
-        $req->execute([$nom, $message]);
+
+    $query = "INSERT INTO messages (nom, message, date) VALUES (?, ?, NOW())";
+$getResults = sqlsrv_query($conn, $tsql);
+
 
         echo json_encode(['success' => 'Message envoyé avec succès']);
-    } catch (PDOException $e) {
-        echo json_encode(['error' => 'Erreur d\'insertion dans la base de données']);
-    }
-}
-
-$pdo = null;
+  sqlsrv_free_stmt($getResults);
+sqlsrv_close($conn);
 ?>
